5e8ba7cfafe1b1dfbb2da3f922dd996e9408b280
Alexandre Tual <alexandre.tual@link-value.fr>
2019-11-29 15:34:54 +0100

    chore/monologue

--- src/Component/builder/HandleErrorFormTrait.php
+++ src/Component/builder/HandleErrorFormTrait.php
@@ -4,11 +4,19 @@
 namespace App\Component\builder;
 
 use App\CustomException\ValidatorException;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 trait HandleErrorFormTrait
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * @param FormInterface $form
      */
@@ -20,8 +28,10 @@ trait HandleErrorFormTrait
             foreach ($form->getErrors(true) as $error) {
                 $errors[$error->getOrigin()->getName()] = $error->getMessage();
             }
+            $this->logger->debug('formIsNotValid', [$errors]);
 
             throw new ValidatorException($errors, Response::HTTP_BAD_REQUEST);
         }
+        $this->logger->debug('formIsValid', [true]);
     }
 }
