bbbea9b28a15cc656e67779556d20a43b24924c6
Alexandre Tual <alexandre.tual@link-value.fr>
2019-12-04 15:24:43 +0100

    feat/skill-entity

---
+++ src/Entity/Category.php
@@ -0,0 +1,98 @@
+<?php
+
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+class Category implements EntityInterface
+{
+    use EntityTrait;
+
+    /** @var string */
+    private $name;
+
+    /** @var ArrayCollection */
+    private $skills;
+
+    /** @var int */
+    private $parentId;
+
+    public function __construct()
+    {
+        $this->skills = new ArrayCollection();
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Category
+     */
+    public function setName(string $name): Category
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Skill[]
+     */
+    public function getSkills(): Collection
+    {
+        return $this->skills;
+    }
+
+    /**
+     * @param Skill $skill
+     * @return $this
+     */
+    public function addSkill(Skill $skill): self
+    {
+        if (!$this->skills->contains($skill)) {
+            $this->skills[] = $skill;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param Skill $skill
+     * @return $this
+     */
+    public function removeSkill(Skill $skill): self
+    {
+        if ($this->skills->contains($skill)) {
+            $this->skills->removeElement($skill);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getParentId(): ?int
+    {
+        return $this->parentId;
+    }
+
+    /**
+     * @param int $parentId
+     * @return Category
+     */
+    public function setParentId(int $parentId): Category
+    {
+        $this->parentId = $parentId;
+
+        return $this;
+    }
+}
