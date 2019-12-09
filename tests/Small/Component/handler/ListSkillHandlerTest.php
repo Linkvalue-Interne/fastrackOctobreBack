5004f0c718b15f6561c10d56190ca269ed22bfac
Alexandre Tual <alexandre.tual@link-value.fr>
2019-12-09 13:25:55 +0100

    feat/skill-handler

---
+++ tests/Small/Component/handler/ListSkillHandlerTest.php
@@ -0,0 +1,76 @@
+<?php
+
+
+namespace App\Tests\Small\Component\handler;
+
+use App\Component\builder\SkillBuilder;
+use App\Component\handler\ListSkillHandler;
+use App\Component\retrieveAll\CategoryRetriever;
+use App\Entity\Category;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class ListSkillHandlerTest extends TestCase
+{
+    private $retriever;
+
+    private $builder;
+
+    private $request;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->retriever = $this->createMock(CategoryRetriever::class);
+        $this->builder = $this->createMock(SkillBuilder::class);
+        $this->request = $this->createMock(Request::class);
+    }
+
+    public function init()
+    {
+        return new ListSkillHandler($this->retriever, $this->builder);
+    }
+
+    public function testHandleSuccess()
+    {
+        $category = $this->createMock(Category::class);
+        $listCategories = [$category, $category];
+        $expect = ['listFormatted'];
+
+        $this->retriever
+            ->expects($this->once())
+            ->method('getAll')
+            ->willReturn($listCategories);
+
+        $this->builder
+            ->expects($this->once())
+            ->method('formatList')
+            ->with($listCategories)
+            ->willReturn($expect);
+
+        $actual = $this->init()->handle($this->request);
+
+        $this->assertSame($expect, $actual);
+    }
+
+    public function testHandleReturnEmptyArray()
+    {
+        $listCategories = [];
+        $expect = [];
+
+        $this->retriever
+            ->expects($this->once())
+            ->method('getAll')
+            ->willReturn($listCategories);
+
+        $this->builder
+            ->expects($this->once())
+            ->method('formatList')
+            ->with($listCategories)
+            ->willReturn($expect);
+
+        $actual = $this->init()->handle($this->request);
+
+        $this->assertSame($expect, $actual);
+    }
+}
