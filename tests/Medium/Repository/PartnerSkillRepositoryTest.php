0d30593f7b5c38b71b9725db1f8afd774af7207c
Alexandre Tual <alexandre.tual@link-value.fr>
2019-12-18 00:33:27 +0100

    feat/partnerSkill-writer-repo

---
+++ tests/Medium/Repository/PartnerSkillRepositoryTest.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Tests\Medium\Repository;
+
+use App\Entity\PartnerSkill;
+use App\Repository\PartnerRepository;
+use App\Repository\PartnerSkillRepository;
+use App\Repository\SkillRepository;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class PartnerSkillRepositoryTest extends KernelTestCase
+{
+    use RepositoryTraitTest;
+
+    public function init()
+    {
+        return new PartnerSkill();
+    }
+
+    public function testRepositoryTest()
+    {
+        $entity = $this->init();
+
+        $partner = self::$kernel->getContainer()->get(PartnerRepository::class)->findOneById(1);
+
+        $skill = self::$kernel->getContainer()->get(SkillRepository::class)->findOneById(1);
+
+        $entity
+            ->setPartner($partner)
+            ->setSkill($skill)
+            ->setLevel(99)
+        ;
+
+        $actual = self::$kernel->getContainer()
+            ->get(PartnerSkillRepository::class)
+            ->save($entity);
+
+        $this->assertSame($entity, $actual);
+
+        $this->initialState($actual);
+    }
+}
